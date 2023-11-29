89e1aa5aec173391d088994b4d18e6a95f21c59f
DebugManager <150149660+AlgorithmAce1@users.noreply.github.com>
2023-11-29 16:54:24 +0200

    change routes back

--- src/backend/app/app/api/api_v1/endpoints/utils.py
+++ src/backend/app/app/api/api_v1/endpoints/utils.py
@@ -23,7 +23,7 @@ def test_celery(
     return {"msg": "Word received"}
 
 
-@router.post("/test-email/", response_model=schemas.Msg, status_code=201)
+@router.post("/test-email/user/", response_model=schemas.Msg, status_code=201)
 def test_email(
     email_to: EmailStr,
     current_user: models.User = Depends(deps.get_current_active_superuser),
